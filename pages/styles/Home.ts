071baa236ad8bf21ac3bee2f6ed3177a7b515d17
aarchiiive <wychrodjxz691@gmail.com>
2023-11-21 17:43:19 +0900

    add : Add styles for logo and content container

--- pages/styles/Home.ts
+++ pages/styles/Home.ts
@@ -1,22 +1,58 @@
 import { Image, StyleSheet, View, TouchableOpacity, useColorScheme } from "react-native";
-import { Color } from "./global/Color"
+import { Color } from "./global/Color";
+import { globalStyles } from "./global/GlobalStyles";
+import { FontFamily } from "./global/Font";
 
 
 export const styles = StyleSheet.create({
-  child: {
+  logo: {
     width: 72,
     height: 103,
   },
   lightView: {
     backgroundColor: Color.lightModeBackground,
     flex: 1,
+    height: '100%',
     justifyContent: 'center',  // 중앙 정렬 (세로)
     alignItems: 'center',      // 중앙 정렬 (가로)
+  },
+  contentContainer: {
+    paddingVertical: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   darkView: {
     backgroundColor: Color.darkModeBackground,
     flex: 1,
     justifyContent: 'center',  // 중앙 정렬 (세로)
     alignItems: 'center',      // 중앙 정렬 (가로)
+  },
+
+  button: {
+    width: 160,
+    height: 40,
+    borderRadius: 10,
+    marginVertical: 20,
+    backgroundColor: Color.lightModeWhite,
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...globalStyles.buttonShadow,
+  },
+  buttonText: {
+    fontFamily: FontFamily.poppinsMedium,
+    fontSize: 14,
+    alignSelf: 'center',
+  },
+
+  developerModeButton: {
+    marginVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  developerModeText: {
+    fontFamily: FontFamily.poppinsMedium,
+    color: Color.lightModeGray,
+    fontSize: 14,
+    alignSelf: 'center',
   },
 });
